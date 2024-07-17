c6bd180510144d193345c4feb649167ff5601b9f
joaquingutierrez <joaquinguty@gmail.com>
2024-07-17 14:51:22 -0300

    Agregada animaciones a los botones

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,12 @@ import "./style.css"
 const Button = ({title, isSubmit}) => {
 
     return (
-        <button className="customButton" type={isSubmit ? "submit" : ""}>{title}</button>
+        <button className="customButton" type={isSubmit ? "submit" : ""}>
+            <span>{title}</span>
+{/*             <div className="customButton-alter">
+                <p>{title}</p>
+            </div> */}
+        </button>
     )
 }
 
