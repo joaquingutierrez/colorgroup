6f3d360d15d8074707bcae4c80adbc013a1755d5
joaquingutierrez <joaquinguty@gmail.com>
2024-07-18 10:25:50 -0300

    Fix: Ahora el logo del navbar funciona como link para ir a la home

--- src/componentsContainer/Header/index.jsx
+++ src/componentsContainer/Header/index.jsx
@@ -1,12 +1,13 @@
 import "./style.css"
 import { LangButton, NavBar } from "../../components"
 import colorGroup_logo from "../../assets/colorGroup_logo.png"
+import { Link } from "react-router-dom"
 
 const Header = ({links}) => {
 
     return (
         <header className="headerContainer">
-            <img src={colorGroup_logo} alt="Color Grupo Logo" />
+            <Link to="/"><img src={colorGroup_logo} alt="Color Grupo Logo" /></Link>
             <div className="headerContainer-utilsContainer">
                 <LangButton />
                 <NavBar links={links} />
