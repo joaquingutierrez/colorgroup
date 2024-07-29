0ade167a21dac9b6af0fd36205f38e7d92534046
joaquingutierrez <joaquinguty@gmail.com>
2024-07-29 10:25:16 -0300

    Responsive para Services terminado

--- src/componentsContainer/Enhance/index.jsx
+++ src/componentsContainer/Enhance/index.jsx
@@ -1,14 +1,17 @@
 import "./style.css"
 import { Button } from "../../components"
+import useWindowSize from "../../customHooks/useWindowSize"
 
 const Enhance = () => {
+
+    const { width } = useWindowSize()
 
     return (
         <section className="enhanceContainer">
             <div className="enhance-textContainer">
                 <h2>¡Descubra cómo podemos potenciar su negocio hoy mismo!</h2>
                 <p>Estamos aquí para ayudarle a alcanzar sus objetivos.</p>
-                <Button title={"Empieza"} />
+                {width < 1025 ? <button className="enchanceContainer-customButton">Empieza</button> : <Button title={"Empieza"} />}
             </div>
         </section>
     )
