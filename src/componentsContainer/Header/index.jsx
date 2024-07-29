d8b23ac6b6409313fe9cbb74001a3fa168d43e46
joaquingutierrez <joaquinguty@gmail.com>
2024-07-29 18:15:54 -0300

    Responsive para Header Terminado

--- src/componentsContainer/Header/index.jsx
+++ src/componentsContainer/Header/index.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 import "./style.css"
 import { LangButton, NavBar } from "../../components"
 import colorGroup_logo from "../../assets/colorGroup_logo.png"
 import { Link } from "react-router-dom"
 
-const Header = ({links}) => {
+const Header = ({ links }) => {
+
+    const [enterUtils, setEnterUtils] = useState(false)
+
+
+    const handleClick = () => {
+        setEnterUtils(!enterUtils)
+    }
 
     return (
         <header className="headerContainer">
             <Link to="/"><img src={colorGroup_logo} alt="Color Grupo Logo" /></Link>
-            <div className="headerContainer-utilsContainer">
+            <div className={`header-mobileButton ${enterUtils ? "" : ""}`} onClick={handleClick}>
+                <div className="header-mobileButton-line header-mobileButton-topLine"></div>
+                <div className="header-mobileButton-line header-mobileButton-bottomLine"></div>
+            </div>
+            <div className={`headerContainer-utilsContainer ${enterUtils ? "headerContainer-utilsContainer-enter" : ""}`}>
                 <LangButton />
                 <NavBar links={links} />
             </div>
