9cb8b7daed0ee03851b2f57395216424ba0a5a77
joaquingutierrez <joaquinguty@gmail.com>
2024-07-29 18:25:54 -0300

    Agregada animacion al button del Header en mobile

--- src/componentsContainer/Header/index.jsx
+++ src/componentsContainer/Header/index.jsx
@@ -17,7 +17,7 @@ const Header = ({ links }) => {
     return (
         <header className="headerContainer">
             <Link to="/"><img src={colorGroup_logo} alt="Color Grupo Logo" /></Link>
-            <div className={`header-mobileButton ${enterUtils ? "" : ""}`} onClick={handleClick}>
+            <div className={`header-mobileButton ${enterUtils ? "header-mobileButton-active" : ""}`} onClick={handleClick}>
                 <div className="header-mobileButton-line header-mobileButton-topLine"></div>
                 <div className="header-mobileButton-line header-mobileButton-bottomLine"></div>
             </div>
